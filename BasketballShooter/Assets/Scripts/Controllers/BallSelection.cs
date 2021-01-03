e3033ef38b431007750af92509af5ad54df360de
ugurinal <ugurinal01@hotmail.com>
2021-01-03 15:23:57 +0300

    BasketballShooter

---
+++ BasketballShooter/Assets/Scripts/Controllers/BallSelection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallSelection : MonoBehaviour
+{
+    [SerializeField] private List<Button> balls = new List<Button>();
+
+    private void Awake()
+    {
+        for (int i = 0; i < balls.Count; i++)
+        {
+            balls[i].onClick.RemoveAllListeners();
+            balls[i].onClick.AddListener(() => SelectBall());
+        }
+    }
+
+    private void SelectBall()
+    {
+        int index = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+
+        Debug.Log("Selected Ball is " + index);
+    }
+}
---
+++ BasketballShooter/Assets/Scripts/Controllers/MainMenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [Header("Panels")]
+    [SerializeField] private Animator mainButtonHolder;
+    [SerializeField] private Animator ballSelectPanel;
+
+    [Header("Buttons")]
+    [Space(20)]
+    [SerializeField] private Button ballsButton;
+    [SerializeField] private Button backButton;
+
+    private void Awake()
+    {
+        ballsButton.onClick.RemoveAllListeners();
+        backButton.onClick.RemoveAllListeners();
+
+        ballsButton.onClick.AddListener(() => ShowBallSelect());
+        backButton.onClick.AddListener(() => ShowMainButtons());
+    }
+
+    private void ShowBallSelect()
+    {
+        mainButtonHolder.SetTrigger("FadeOut");
+        ballSelectPanel.SetTrigger("FadeIn");
+    }
+
+    private void ShowMainButtons()
+    {
+        ballSelectPanel.SetTrigger("FadeOut");
+        mainButtonHolder.SetTrigger("FadeIn");
+    }
+}
