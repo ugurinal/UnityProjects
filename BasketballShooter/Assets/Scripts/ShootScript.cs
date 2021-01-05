d95095af448e4f91bfe6394e4950d4cd14b47c3d
ugurinal <ugurinal01@hotmail.com>
2021-01-05 21:55:10 +0300

    BasketballShooter
    shootscript

---
+++ BasketballShooter/Assets/Scripts/ShootScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ShootScript : MonoBehaviour
+{
+    public float power = 2.0f;
+    public float life = 1.0f;
+    public float deadSense = 25f;
+
+    private Vector2 startPos;
+
+    private bool shoot = false;
+    private bool aiming = false;
+    private bool hitGround = false;
+
+    [SerializeField] private List<GameObject> projectilesPath;
+
+    private Rigidbody2D myBody;
+    private Collider2D myCollider;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
