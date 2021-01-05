e27ff5012a3585cf15cc3aa8c3dba266f65b363e
ugurinal <ugurinal01@hotmail.com>
2021-01-05 19:49:57 +0300

    BasketballShooter
    ball spawner

---
+++ BasketballShooter/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    private GameObject ball;
+
+    private float minX = -4.7f;
+    private float maxX = 8f;
+    private float minY = -2.5f;
+    private float maxY = 1.5f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        float ballX = Random.Range(minX, maxX);
+        float ballY = Random.Range(minY, maxY);
+
+        ball = Instantiate(GameManager.Instance.Balls[GameManager.Instance.SelectedBallIndex], new Vector3(ballX, ballY, 0), Quaternion.identity);
+
+        ball.GetComponent<Rigidbody2D>().isKinematic = true;
+    }
+}
---
+++ BasketballShooter/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance { get { return _instance; } }
+
+    private int _selectedBallIndex; // ball index
+
+    public int SelectedBallIndex { get { return _selectedBallIndex; } set { _selectedBallIndex = value; } }
+
+    [SerializeField] private List<GameObject> _balls;    // ball prefabs
+
+    public List<GameObject> Balls { get { return _balls; } }
+
+    private void Awake()
+    {
+        MakeSingleton();
+    }
+
+    private void MakeSingleton()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
