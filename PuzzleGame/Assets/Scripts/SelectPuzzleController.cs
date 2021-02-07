16ae16c9d24eddf2c130a298dcc54405d09f03e0
UGURINAL <ugurinal01@hotmail.com>
2021-02-07 17:53:34 +0300

    bug?
    +bug: ??

---
+++ PuzzleGame/Assets/Scripts/SelectPuzzleController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class SelectPuzzleController : MonoBehaviour
+{
+    public void SelectPuzzle()
+    {
+        string[] name = EventSystem.current.currentSelectedGameObject.name.Split(' ');
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
