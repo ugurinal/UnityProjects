893f1561ccba88dde300a574af11d9fd89251939
UGURINAL <ugurinal01@hotmail.com>
2021-02-12 01:13:25 +0300

    update
    +new: Shuffle
    +new: Slide

--- NewPuzzleGame/Assets/Scripts/GamePlay.cs
+++ NewPuzzleGame/Assets/Scripts/GamePlay.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GamePlay : MonoBehaviour
 {
+    [SerializeField] private GameObject[] _elements;
+    [SerializeField] private GameObject _emptySpace;
+
+    private void Start()
+    {
+        Shuffle();
+    }
+
     private void Update()
     {
         CheckInput();
@@ -16,12 +22,27 @@ public class GamePlay : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
 
-            Debug.Log("TEST");
-
-            if (hit.collider != null)
+            if (hit)
             {
-                Debug.Log(hit.collider.gameObject.name);
+                if (Vector2.Distance(hit.transform.position, _emptySpace.transform.position) < 2)
+                {
+                    Vector2 lastEmptySpace = _emptySpace.transform.position;
+                    _emptySpace.transform.position = hit.transform.position;
+                    hit.transform.position = lastEmptySpace;
+                }
             }
+        }
+    }
+
+    private void Shuffle()
+    {
+        for (int i = 0; i < _elements.Length; i++)
+        {
+            int random = Random.Range(0, _elements.Length);
+
+            Vector2 original = _elements[i].transform.position;
+            _elements[i].transform.position = _elements[random].transform.position;
+            _elements[random].transform.position = original;
         }
     }
 }
