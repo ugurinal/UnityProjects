562bcd4ffd34974bef40e6ad8c5108417abf8cd7
UGURINAL <ugurinal01@hotmail.com>
2021-02-12 00:14:15 +0300

    Sliding Puzzle Game
    +init

---
+++ NewPuzzleGame/Assets/Scripts/GamePlay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlay : MonoBehaviour
+{
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+            Debug.Log("TEST");
+
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+            }
+        }
+    }
+}
