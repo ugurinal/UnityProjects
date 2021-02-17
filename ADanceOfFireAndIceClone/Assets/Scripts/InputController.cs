56bcebccc35f46c38fa787adbddaa77ba76a565c
UGURINAL <ugurinal01@hotmail.com>
2021-02-17 03:40:28 +0300

    ADanceOfFireAndIceClone
    +added: path for player to follow

--- ADanceOfFireAndIceClone/Assets/Scripts/InputController.cs
+++ ADanceOfFireAndIceClone/Assets/Scripts/InputController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class InputController : MonoBehaviour
 {
+    [SerializeField] private GameObject[] _path;
+
     [SerializeField] private GameObject _fire;
     [SerializeField] private GameObject _ice;
 
@@ -11,9 +13,12 @@ public class InputController : MonoBehaviour
 
     private bool _isFire = false;
 
+    private int _currentBlock = 0;
+
     private void Start()
     {
-        _isFire = Random.value >= 0.5;
+        _isFire = false;
+        //_isFire = Random.value >= 0.5;
     }
 
     private void Update()
@@ -22,11 +27,11 @@ public class InputController : MonoBehaviour
 
         if (_isFire)
         {
-            _fire.transform.RotateAround(_ice.transform.position, Vector3.forward, _rotationSpeed * Time.deltaTime);
+            _fire.transform.RotateAround(_ice.transform.position, Vector3.back, _rotationSpeed * Time.deltaTime);
         }
         else
         {
-            _ice.transform.RotateAround(_fire.transform.position, Vector3.forward, _rotationSpeed * Time.deltaTime);
+            _ice.transform.RotateAround(_fire.transform.position, Vector3.back, _rotationSpeed * Time.deltaTime);
         }
     }
 
@@ -34,7 +39,31 @@ public class InputController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
-            _isFire = !_isFire;
+            if (_isFire)
+            {
+                Debug.Log("Fire distance to block : " + Vector2.Distance(_fire.transform.position, _path[_currentBlock].transform.position));
+                if (Vector2.Distance(_fire.transform.position, _path[_currentBlock].transform.position) < 0.3f)
+                {
+                    Debug.Log("FIRE!");
+                    _fire.transform.position = _path[_currentBlock].transform.position;
+                    _currentBlock++;
+                    _isFire = !_isFire;
+                }
+            }
+            else
+            {
+                Debug.Log("Ice distance to block : " + Vector2.Distance(_ice.transform.position, _path[_currentBlock].transform.position));
+
+                if (Vector2.Distance(_ice.transform.position, _path[_currentBlock].transform.position) < 0.3f)
+                {
+                    Debug.Log("ICE!");
+                    _ice.transform.position = _path[_currentBlock].transform.position;
+                    _currentBlock++;
+                    _isFire = !_isFire;
+                }
+            }
+
+            //_isFire = !_isFire;
         }
     }
 }   // Input Controller
