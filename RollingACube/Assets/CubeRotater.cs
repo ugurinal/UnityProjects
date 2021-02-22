47fb2077223e515d9054d02be1586816415a8ba9
UGURINAL <ugurinal01@hotmail.com>
2021-02-23 00:03:10 +0300

    Rolling A Cube
    +init

---
+++ RollingACube/Assets/CubeRotater.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotater : MonoBehaviour
+{
+    public float tumblingDuration = 0.2f;
+
+    private void Update()
+    {
+        var dir = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            dir = Vector3.forward;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            dir = Vector3.back;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            dir = Vector3.left;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            dir = Vector3.right;
+
+        if (dir != Vector3.zero && !isTumbling)
+        {
+            StartCoroutine(Tumble(dir));
+        }
+    }
+
+    private bool isTumbling = false;
+
+    private IEnumerator Tumble(Vector3 direction)
+    {
+        isTumbling = true;
+
+        var rotAxis = Vector3.Cross(Vector3.up, direction);
+        var pivot = (transform.position + Vector3.down * 0.5f) + direction * 0.5f;
+
+        var startRotation = transform.rotation;
+        var endRotation = Quaternion.AngleAxis(90.0f, rotAxis) * startRotation;
+
+        var startPosition = transform.position;
+        var endPosition = transform.position + direction;
+
+        var rotSpeed = 90.0f / tumblingDuration;
+        var t = 0.0f;
+
+        while (t < tumblingDuration)
+        {
+            t += Time.deltaTime;
+            if (t < tumblingDuration)
+            {
+                transform.RotateAround(pivot, rotAxis, rotSpeed * Time.deltaTime);
+                yield return null;
+            }
+            else
+            {
+                transform.rotation = endRotation;
+                transform.position = endPosition;
+            }
+        }
+
+        isTumbling = false;
+    }
+}
---
+++ RollingACube/Assets/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _rollSpeed = 0.5f;
+    private bool isRolling = false;
+
+    private void Update()
+    {
+        if (isRolling)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            isRolling = true;
+            StartCoroutine(Rotate(transform.position + new Vector3(0f, -0.5f, 0.5f), Vector3.right));
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            isRolling = true;
+            StartCoroutine(Rotate(transform.position + new Vector3(0f, -0.5f, -0.5f), Vector3.left));
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            isRolling = true;
+            StartCoroutine(Rotate(transform.position + new Vector3(-0.5f, -0.5f, 0f), Vector3.forward));
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            isRolling = true;
+            StartCoroutine(Rotate(transform.position + new Vector3(0.5f, -0.5f, 0f), Vector3.back));
+        }
+    }
+
+    private IEnumerator Rotate(Vector3 pivot, Vector3 direction)
+    {
+        for (int i = 1; i <= 90 / _rollSpeed; i++)
+        {
+            transform.RotateAround(pivot, direction, _rollSpeed);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        transform.position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), Mathf.Round(transform.position.z));
+
+        isRolling = false;
+    }
+
+    /*
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision ENTER");
+        GetComponent<Rigidbody>().drag = float.MaxValue;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Collision STAY");
+
+        GetComponent<Rigidbody>().drag = float.MaxValue;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Collision EXIT");
+
+        GetComponent<Rigidbody>().drag = 0f;
+    }*/
+}
