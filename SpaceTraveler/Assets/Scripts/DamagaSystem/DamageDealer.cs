1af2d256cb279570ab81d8a6a388e163346372af
UGURINAL <ugurinal01@hotmail.com>
2021-02-27 18:22:03 +0300

    Space Traveler
    +rafactoring

--- SpaceTraveler/Assets/Scripts/DamagaSystem/DamageDealer.cs
+++ SpaceTraveler/Assets/Scripts/DamagaSystem/DamageDealer.cs
@@ -2,36 +2,36 @@
 
 namespace SpaceTraveler.DamageSystem
 {
+    #region DESCRIPTION
+
+    /// <summary>
+    /// This is the main class for dealing damage to enemies or player
+    /// This script is attached to gameobjects that should deal damage like enemy projectile,
+    /// player projectile, enemy itself and player itself
+    /// </summary>
+
+    #endregion DESCRIPTION
+
     public class DamageDealer : MonoBehaviour
     {
-        #region DESCRIPTION
-
-        //  *********************************************************************************************
-        //  * This is the main class for dealing damage to enemies or player                            *
-        //  * This script is attached to gameobjects that should deal damage like enemy projectile,     *
-        //  * player projectile, enemy itself and player itself                                         *
-        //  *********************************************************************************************
-
-        #endregion DESCRIPTION
-
         #region FIELDS
 
         [Header("Damage Properties")]
-        [SerializeField] private float damage = 100;
+        [SerializeField] private float _damage = 100;
 
-        public float Damage { get => damage; }
+        public float Damage { get => _damage; }
 
         #endregion FIELDS
 
         private void Start()
         {
-            // Below function checks if this script attached to a player projectile or not,
-            // if it is, it adjusts the damage by player ship
+            // checks if this script attached to a player projectile or not,
+            // if it is, it updates the damage by player ship
             // because every player ship has its own damage multiplier
 
             if (tag == "PlayerProjectile")
             {
-                damage *= Player.instance.GetDamage();
+                _damage *= Player.instance.GetDamage();
             }
         }
 
