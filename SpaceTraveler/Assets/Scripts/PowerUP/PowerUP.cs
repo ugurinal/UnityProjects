77e1e46a4fb749a6e35fd596dc70c8d90788ed68
UGURINAL <ugurinal01@hotmail.com>
2021-03-03 19:08:57 +0300

    Space Traveler
    +added: shooting type and laser materials

--- SpaceTraveler/Assets/Scripts/PowerUP/PowerUP.cs
+++ SpaceTraveler/Assets/Scripts/PowerUP/PowerUP.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using SpaceTraveler.Player;
 
 namespace SpaceTraveler.PowerUPSystem
 {
     public class PowerUP : MonoBehaviour
     {
         public List<GameObject> Projectile = null;
+        public PlayerProperties.ShootingTypes ShootingTypes;
+        public List<Material> LaserMaterials = null;
     }
 }
