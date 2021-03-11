c26c8fd9980abd1119c43cdfc68ae094babb9296
UGURINAL <ugurinal01@hotmail.com>
2021-03-11 20:20:46 +0300

    Space Traveler
    +updated: ui in level selection scene updated

--- SpaceTraveler/Assets/Scripts/LevelSystem/LevelSelector.cs
+++ SpaceTraveler/Assets/Scripts/LevelSystem/LevelSelector.cs
@@ -7,7 +7,10 @@ namespace SpaceTraveler.LevelSystem
     public class LevelSelector : MonoBehaviour
     {
         [Header("Level Buttons")]
-        [SerializeField] private Button[] _levelButtons;
+        [SerializeField] private GameObject[] _levelButtons;
+
+        [Header("Level Button Sprites")]
+        [SerializeField] private Sprite[] _levelButtonSprites;
 
         private GameManager _gameManager = null;
 
@@ -36,7 +39,7 @@ namespace SpaceTraveler.LevelSystem
 
                     if (i == levelReached - 1)
                     {
-                        _levelButtons[i].transform.GetChild(8).gameObject.SetActive(true);
+                        _levelButtons[i].transform.GetChild(2).gameObject.SetActive(true);
                     }
                 }
             }
@@ -49,44 +52,20 @@ namespace SpaceTraveler.LevelSystem
             switch (starCount)
             {
                 case 0:
-                    _levelButtons[levelIndex].transform.GetChild(2).gameObject.SetActive(true);
-                    _levelButtons[levelIndex].transform.GetChild(3).gameObject.SetActive(true);
-                    _levelButtons[levelIndex].transform.GetChild(4).gameObject.SetActive(true);
-
-                    _levelButtons[levelIndex].transform.GetChild(5).gameObject.SetActive(false);
-                    _levelButtons[levelIndex].transform.GetChild(6).gameObject.SetActive(false);
-                    _levelButtons[levelIndex].transform.GetChild(7).gameObject.SetActive(false);
+                    // do nothing
                     break;
 
                 case 1:
-                    _levelButtons[levelIndex].transform.GetChild(2).gameObject.SetActive(false);
-                    _levelButtons[levelIndex].transform.GetChild(3).gameObject.SetActive(true);
-                    _levelButtons[levelIndex].transform.GetChild(4).gameObject.SetActive(true);
-
-                    _levelButtons[levelIndex].transform.GetChild(5).gameObject.SetActive(true);
-                    _levelButtons[levelIndex].transform.GetChild(6).gameObject.SetActive(false);
-                    _levelButtons[levelIndex].transform.GetChild(7).gameObject.SetActive(false);
+                    _levelButtons[levelIndex].GetComponent<Image>().sprite = _levelButtonSprites[0];
                     break;
 
                 case 2:
-                    _levelButtons[levelIndex].transform.GetChild(2).gameObject.SetActive(false);
-                    _levelButtons[levelIndex].transform.GetChild(3).gameObject.SetActive(false);
-                    _levelButtons[levelIndex].transform.GetChild(4).gameObject.SetActive(true);
-
-                    _levelButtons[levelIndex].transform.GetChild(5).gameObject.SetActive(true);
-                    _levelButtons[levelIndex].transform.GetChild(6).gameObject.SetActive(true);
-                    _levelButtons[levelIndex].transform.GetChild(7).gameObject.SetActive(false);
+                    _levelButtons[levelIndex].GetComponent<Image>().sprite = _levelButtonSprites[1];
 
                     break;
 
                 case 3:
-                    _levelButtons[levelIndex].transform.GetChild(2).gameObject.SetActive(false);
-                    _levelButtons[levelIndex].transform.GetChild(3).gameObject.SetActive(false);
-                    _levelButtons[levelIndex].transform.GetChild(4).gameObject.SetActive(false);
-
-                    _levelButtons[levelIndex].transform.GetChild(5).gameObject.SetActive(true);
-                    _levelButtons[levelIndex].transform.GetChild(6).gameObject.SetActive(true);
-                    _levelButtons[levelIndex].transform.GetChild(7).gameObject.SetActive(true);
+                    _levelButtons[levelIndex].GetComponent<Image>().sprite = _levelButtonSprites[2];
                     break;
 
                 default:
