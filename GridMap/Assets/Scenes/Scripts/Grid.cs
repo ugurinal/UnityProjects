891c10a86c955f0193c05b516e93b483c8df6f46
UGURINAL <ugurinal01@hotmail.com>
2021-03-19 22:42:21 +0300

    Grid System
    +init: a simple grid system

---
+++ GridMap/Assets/Scenes/Scripts/Grid.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+    private int _width;
+    private int _height;
+    private int[,] _gridArray;
+
+    public Grid(int width, int height)
+    {
+        _width = width;
+        _height = height;
+        _gridArray = new int[width, height];
+    }
+}
