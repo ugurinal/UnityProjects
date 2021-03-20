969dd231a5107d8dc83377311fcd9f068adef632
UGURINAL <ugurinal01@hotmail.com>
2021-03-20 13:14:37 +0300

    Grid map
    ++

---
+++ GridMap/Assets/Scenes/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (_target == null)
+            return;
+        UpdateDirection();
+    }
+
+    private void UpdateDirection()
+    {
+        Vector3 direction = _target.position - transform.position;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * 50f);
+    }
+}
