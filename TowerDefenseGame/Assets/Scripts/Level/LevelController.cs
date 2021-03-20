2f62ac37ba8eedc2d06be219810cf31578501ec5
UGURINAL <ugurinal01@hotmail.com>
2021-03-20 23:14:16 +0300

    Tower Defense
    +added: level controller

---
+++ TowerDefenseGame/Assets/Scripts/Level/LevelController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense.Level
+{
+    public class LevelController : MonoBehaviour
+    {
+        private static LevelController _instance;
+
+        public static LevelController Instance { get => _instance; }
+
+        private List<GameObject> _enemies;
+        public List<GameObject> Enemies { get => _enemies; }
+
+        private int _enemySize;
+        private int _enemyLeft;
+
+        private void Awake()
+        {
+            MakeSingleton();
+        }
+
+        private void Start()
+        {
+            _enemies = new List<GameObject>();
+        }
+
+        private void MakeSingleton()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _instance = this;
+            }
+        }
+
+        public void SetEnemySize(int enemySize)
+        {
+            _enemySize = enemySize;
+            _enemyLeft = _enemySize;
+        }
+
+        public void AddEnemyList(GameObject enemy)
+        {
+            _enemies.Add(enemy);
+            Debug.Log("Enemy added!");
+        }
+    }
+}
