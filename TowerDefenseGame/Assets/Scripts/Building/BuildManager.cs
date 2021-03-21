40f764d0defd9d1b8e3a1eddb0e803701a3a313f
UGURINAL <ugurinal01@hotmail.com>
2021-03-21 23:56:30 +0300

    Tower Defense
    +added: tower panel for user to select which tower will be created

--- TowerDefenseGame/Assets/Scripts/Building/BuildManager.cs
+++ TowerDefenseGame/Assets/Scripts/Building/BuildManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace TowerDefense.Building
 {
@@ -11,6 +12,9 @@ namespace TowerDefense.Building
 
         [SerializeField] private List<GameObject> _towers;
         [SerializeField] private Transform _towerParent;
+        [SerializeField] private GameObject _shopPanel;
+
+        private Vector3 _towerPosition;
 
         private void Awake()
         {
@@ -31,18 +35,54 @@ namespace TowerDefense.Building
 
         public void BuildTower(Vector3 position)
         {
-            // show gui then instantiate
-
-            /*GameObject tower =*/
             Instantiate(_towers[0], position, Quaternion.identity, _towerParent);
         }
 
         private void Start()
         {
+            _shopPanel.SetActive(false);
+
+            Button towerOne = _shopPanel.transform.GetChild(0).GetChild(0).GetComponent<Button>();
+            Button towerTwo = _shopPanel.transform.GetChild(0).GetChild(1).GetComponent<Button>();
+            Button towerThree = _shopPanel.transform.GetChild(0).GetChild(2).GetComponent<Button>();
+
+            towerOne.onClick.RemoveAllListeners();
+            towerTwo.onClick.RemoveAllListeners();
+            towerThree.onClick.RemoveAllListeners();
+
+            towerOne.onClick.AddListener(() => CreateTowerOne());
+            towerTwo.onClick.AddListener(() => CreateTowerTwo());
+            towerThree.onClick.AddListener(() => CreateTowerThree());
         }
 
-        private void Update()
+        public void OpenShopPanel(Vector3 position)
         {
+            _towerPosition = position;
+            _shopPanel.transform.position = position + new Vector3(0f, 8f, 0f);
+            _shopPanel.SetActive(true);
+        }
+
+        public void CloseShopPanel()
+        {
+            _shopPanel.SetActive(false);
+        }
+
+        private void CreateTowerOne()
+        {
+            Instantiate(_towers[0], _towerPosition, Quaternion.identity, _towerParent);
+            _shopPanel.SetActive(false);
+        }
+
+        private void CreateTowerTwo()
+        {
+            Instantiate(_towers[1], _towerPosition, Quaternion.identity, _towerParent);
+            _shopPanel.SetActive(false);
+        }
+
+        private void CreateTowerThree()
+        {
+            Instantiate(_towers[2], _towerPosition, Quaternion.identity, _towerParent);
+            _shopPanel.SetActive(false);
         }
     }
 }
