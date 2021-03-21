5f716165fcb664902934ad898b4d612074331ad8
UGURINAL <ugurinal01@hotmail.com>
2021-03-21 16:18:54 +0300

    Tower Defense
    +added: tower building logic (not done yet)

---
+++ TowerDefenseGame/Assets/Scripts/Building/BuildManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    private static BuildManager _instance;
+    public static BuildManager Instance { get => _instance; }
+
+    [SerializeField] private List<GameObject> _towers;
+
+    private void Awake()
+    {
+        MakeSingleton();
+    }
+
+    private void MakeSingleton()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public void BuildTower(Vector3 position)
+    {
+        // show gui then instantiate
+
+        /*GameObject tower =*/
+        Instantiate(_towers[0], position, Quaternion.identity);
+    }
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+}
---
+++ TowerDefenseGame/Assets/Scripts/Building/Node.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [SerializeField] private Color _hoverColor;
+
+    private GameObject _tower;
+
+    private Renderer _renderer;
+    private Color _startColor;
+
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+        _startColor = _renderer.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if (_tower != null)
+        {
+            Debug.Log("You can not place tower!");
+            return;
+        }
+
+        BuildManager.Instance.BuildTower(transform.position);
+    }
+
+    private void OnMouseEnter()
+    {
+        transform.position += new Vector3(0, 1f, 0f);
+        _renderer.material.color = _hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        transform.position -= new Vector3(0, 1f, 0f);
+        _renderer.material.color = _startColor;
+    }
+}
